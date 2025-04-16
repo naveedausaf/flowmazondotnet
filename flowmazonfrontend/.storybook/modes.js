81b21b5dc1b97fce06a969b940a39e07610ddb47
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-04-16 12:30:34 +0100

    fix: setup Chromatic viewports

---
+++ flowmazonfrontend/.storybook/modes.js
@@ -0,0 +1,16 @@
+// .storybook/modes.js
+
+export const allModes = {
+  sm: {
+    viewport: 'sm',
+  },
+  md: {
+    viewport: 'md',
+  },
+  lg: {
+    viewport: 'lg',
+  },
+  xl: {
+    viewport: 'xll',
+  },
+};
