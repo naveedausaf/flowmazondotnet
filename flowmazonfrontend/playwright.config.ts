1060d1d943010a1447a2dd78e455e4c61696710a
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-05-26 13:14:05 +0100

    fix: add playwright tests

--- flowmazonfrontend/playwright.config.ts
+++ flowmazonfrontend/playwright.config.ts
@@ -26,14 +26,18 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL: process.env.LCT
+      ? 'http://127.0.0.1:4000' //URL exposed by locally running container
+      : 'http://127.0.0.1:3000', //Next.js app URL
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'retry-with-trace',
+    screenshot: 'only-on-failure',
+    video: 'retry-with-video',
   },
 
   /* Configure projects for major browsers */
@@ -75,9 +79,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: process.env.LCT ? 'http://127.0.0.1:4000' : 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
