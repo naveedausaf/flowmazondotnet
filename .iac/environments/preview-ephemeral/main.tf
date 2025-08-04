b20af0d4b95fb043f5aa6898877248c5fd2ca0fe
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-08-04 20:43:37 +0100

    fix: chagnes

--- .iac/environments/preview-ephemeral/main.tf
+++ .iac/environments/preview-ephemeral/main.tf
@@ -1,5 +1,6 @@
 module "flowmazonfrontend" {
-  source                                  = "git::https://github.com/EnableHub/flowmazondotnet.git//.iac/modules/flowmazonfrontend?ref=main"
+  source = "../../modules/flowmazonfrontend"
+
   vercel_team_id                          = var.vercel_team_id
   vercel_project_name                     = var.vercel_project_name
   vercel_app_domain_name                  = var.vercel_app_domain_name
@@ -21,25 +22,25 @@ module "flowmazonfrontend" {
 
 }
 
-# module "db_branch" {
-#   source                                     = "git::https://github.com/EnableHub/flowmazondotnet.git//.iac/modules/db_branch?ref=main"
-#   managed_identity_name                      = var.managed_identity_name
-#   managed_identity_resource_group_name       = var.id_and_vault_resource_group_name
-#   vault_name                                 = var.key_vault_name
-#   vault_resource_group_name                  = var.id_and_vault_resource_group_name
-#   vault_secretname_for_connectionstring      = var.vault_secretname_for_connectionstring
-#   secretname_for_psql_owner_connectionstring = var.secretname_for_psql_owner_connectionstring
-#   environmentname_for_secrets_and_variables  = var.environmentname_for_secrets_and_variables
-#   repository_for_secrets_and_variables       = var.repository_for_secrets_and_variables
+module "db_branch" {
+  source                                     = "../../modules/db_branch"
+  managed_identity_name                      = var.managed_identity_name
+  managed_identity_resource_group_name       = var.id_and_vault_resource_group_name
+  vault_name                                 = var.key_vault_name
+  vault_resource_group_name                  = var.id_and_vault_resource_group_name
+  vault_secretname_for_connectionstring      = var.vault_secretname_for_connectionstring
+  secretname_for_psql_owner_connectionstring = var.secretname_for_psql_owner_connectionstring
+  environmentname_for_secrets_and_variables  = var.environmentname_for_secrets_and_variables
+  repository_for_secrets_and_variables       = var.repository_for_secrets_and_variables
 
-#   neon_project_id       = var.neon_project_id
-#   neon_source_branch_id = var.neon_source_branch_id
-#   neon_new_branch_name  = var.neon_new_branch_name
-#   neon_database_name    = var.neon_database_name
-#   neon_app_role         = var.neon_app_role
-#   neon_owner_role       = var.neon_owner_role
+  neon_project_id       = var.neon_project_id
+  neon_source_branch_id = var.neon_source_branch_id
+  neon_new_branch_name  = var.neon_new_branch_name
+  neon_database_name    = var.neon_database_name
+  neon_app_role         = var.neon_app_role
+  neon_owner_role       = var.neon_owner_role
 
-# }
+}
 
 
 
