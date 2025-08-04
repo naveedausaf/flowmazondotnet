083f3bacec5a5a7503765a2482595f24ae6f615d
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-08-04 22:55:08 +0100

    fix: fixed variables in preview-ephemeral TF wkspc

--- .iac/environments/preview-ephemeral/main.tf
+++ .iac/environments/preview-ephemeral/main.tf
@@ -1,19 +1,19 @@
 module "flowmazonfrontend" {
   source = "../../modules/flowmazonfrontend"
 
-  vercel_team_id                          = var.vercel_team_id
-  vercel_project_name                     = var.vercel_project_name
-  vercel_app_domain_name                  = var.vercel_app_domain_name
-  vercel_region_for_server_side_execution = var.vercel_region_for_server_side_execution
-  cloudflare_api_token                    = var.cloudflare_api_token
-  cloudflare_zone_id                      = var.cloudflare_zone_id
-  env_NEXT_PUBLIC_OTEL_ENVIRONMENT        = var.env_NEXT_PUBLIC_OTEL_ENVIRONMENT
-  env_OTEL_EXPORTER_OTLP_ENDPOINT         = var.env_OTEL_EXPORTER_OTLP_ENDPOINT
-  env_OTEL_EXPORTER_OTLP_PROTOCOL         = var.env_OTEL_EXPORTER_OTLP_PROTOCOL
-  env_OTEL_EXPORTER_OTLP_HEADERS          = var.env_OTEL_EXPORTER_OTLP_HEADERS
-  secretname_for_vercel_project_id        = var.secretname_for_vercel_project_id
-  environmentname_for_vercel_project_id   = var.environmentname_for_secrets_and_variables
-  repository_for_vercel_project_id        = var.repository_for_secrets_and_variables
+  vercel_team_id                            = var.vercel_team_id
+  vercel_project_name                       = var.vercel_project_name
+  vercel_app_domain_name                    = var.vercel_app_domain_name
+  vercel_region_for_server_side_execution   = var.vercel_region_for_server_side_execution
+  cloudflare_api_token                      = var.cloudflare_api_token
+  cloudflare_zone_id                        = var.cloudflare_zone_id
+  env_NEXT_PUBLIC_OTEL_ENVIRONMENT          = var.env_NEXT_PUBLIC_OTEL_ENVIRONMENT
+  env_OTEL_EXPORTER_OTLP_ENDPOINT           = var.env_OTEL_EXPORTER_OTLP_ENDPOINT
+  env_OTEL_EXPORTER_OTLP_PROTOCOL           = var.env_OTEL_EXPORTER_OTLP_PROTOCOL
+  env_OTEL_EXPORTER_OTLP_HEADERS            = var.env_OTEL_EXPORTER_OTLP_HEADERS
+  secretname_for_vercel_project_id          = var.secretname_for_vercel_project_id
+  environmentname_for_secrets_and_variables = var.environmentname_for_secrets_and_variables
+  repository_for_secrets_and_variables      = var.repository_for_secrets_and_variables
 
   grafanacloud_frontend_o11y_api_access_token            = var.grafanacloud_frontend_o11y_api_access_token
   grafanacloud_stack_slug                                = var.grafanacloud_stack_slug
@@ -27,7 +27,7 @@ module "db_branch" {
 
   managed_identity_name                      = var.managed_identity_name
   managed_identity_resource_group_name       = var.id_and_vault_resource_group_name
-  vault_name                                 = var.key_vault_name
+  vault_name                                 = var.vault_name
   vault_resource_group_name                  = var.id_and_vault_resource_group_name
   vault_secretname_for_connectionstring      = var.vault_secretname_for_connectionstring
   secretname_for_psql_owner_connectionstring = var.secretname_for_psql_owner_connectionstring
