e427994f209d7e13411d5140990aa1f2f0ae29a5
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-08-04 21:37:39 +0100

    fix: changes

--- .iac/environments/preview-ephemeral/main.tf
+++ .iac/environments/preview-ephemeral/main.tf
@@ -1,5 +1,5 @@
 module "flowmazonfrontend" {
-  source = "github.com/EnableHub/flowmazondotnet//.iac/modules/flowmazonfrontend?ref=main"
+  source = "github.com/EnableHub/flowmazondotnet//.iac/modules/flowmazonfrontend?ref=create-uat"
 
   vercel_team_id                          = var.vercel_team_id
   vercel_project_name                     = var.vercel_project_name
@@ -21,4 +21,30 @@ module "flowmazonfrontend" {
   secretname_for_grafanacloud_frontend_o11y_instance_url = var.secretname_for_grafanacloud_frontend_o11y_instance_url
 
 }
+
+module "db_branch" {
+  source = "github.com/EnableHub/flowmazondotnet//.iac/modules/db_branch?ref=create-uat"
+
+  managed_identity_name                      = var.managed_identity_name
+  managed_identity_resource_group_name       = var.id_and_vault_resource_group_name
+  vault_name                                 = var.key_vault_name
+  vault_resource_group_name                  = var.id_and_vault_resource_group_name
+  vault_secretname_for_connectionstring      = var.vault_secretname_for_connectionstring
+  secretname_for_psql_owner_connectionstring = var.secretname_for_psql_owner_connectionstring
+  environmentname_for_secrets_and_variables  = var.environmentname_for_secrets_and_variables
+  repository_for_secrets_and_variables       = var.repository_for_secrets_and_variables
+
+  neon_project_id       = var.neon_project_id
+  neon_source_branch_id = var.neon_source_branch_id
+  neon_new_branch_name  = var.neon_new_branch_name
+  neon_database_name    = var.neon_database_name
+  neon_app_role         = var.neon_app_role
+  neon_owner_role       = var.neon_owner_role
+
+}
+
+
+
+
+
 
