d95b18cb8f832061f676837bce4d374f35d6647b
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-08-05 15:18:17 +0100

    fix: new branch (#124)

--- .iac/environments/preview-ephemeral/variables.tf
+++ .iac/environments/preview-ephemeral/variables.tf
@@ -1,43 +1,4 @@
-
-
 # vars for db_branch module
-variable "managed_identity_name" {
-  description = "Name of the user-assigned managed identity to which read permissions would be assigned to the key vault secret that this module would create."
-  type        = string
-}
-
-
-
-variable "vault_name" {
-  description = "The name of the key vault into which the key vault secrets crated by this module would be stored."
-  type        = string
-}
-
-variable "id_and_vault_resource_group_name" {
-  description = "Name of the Azure resource group in which the vault and user-assinged managed identity exists."
-  type        = string
-}
-
-variable "vault_secretname_for_connectionstring" {
-  description = "name of the secret whose value is the connection string to be used by the API to connect to the database"
-  type        = string
-}
-
-variable "secretname_for_psql_owner_connectionstring" {
-  description = "Name of the GitHub Environment secret in which to store psql connection String that includes credentials of db owner account that would be used to connect to and execute queries (including DDL) on the database from psql."
-  type        = string
-}
-
-variable "environmentname_for_secrets_and_variables" {
-  description = "Name of GitHub repo Environment in which to store secrets and variables such as psql_owner_connection_string."
-  type        = string
-}
-
-variable "repository_for_secrets_and_variables" {
-  description = "Name of GitHub repo in an an Environment of which (specified by `environmentname_for_secrets_and_variables` argument) secrets and variables such as psql_owner_connection_string would be stored. This should just be the repo's name, and NOT prefixed by account name or organisation name and NOT a fully qualified repo name either. Otherwise the repo may not be found. For example I would provide `flowmazondotnet` as the repo name instead of `EnableHub/flowmazondotnet` or `https://github.com/EnableHub/flowmazondotnet`."
-  type        = string
-}
-
 variable "neon_project_id" {
   description = "project_id of the Neon DB Project in which a new branch would be created by this module. This is also the project that contains the source branch off of which the new branch will be created."
   type        = string
@@ -49,28 +10,9 @@ variable "neon_source_branch_id" {
 }
 
 variable "neon_new_branch_name" {
-  description = "Name of the new branch that would be created in the specified Neon DB Project. Credentials for this branch would be stored (in key vault as app connection string andin GitHub Environment as owner connection string for use from psql to run migrations etc.)"
+  description = "Name of the new branch that would be created in the specified Neon DB Project. Credentials for this branch would be stored (in key vault as app connection string and in GitHub Environment as owner connection string for use from psql to run migrations etc.)"
   type        = string
 }
-
-variable "neon_database_name" {
-  description = "The name of the database in the specified Neon DB project. Permissions would be assigned in this database to the app role which this module would create on the new branch. Also, connection strings for connecting to this database, from app and owner role respectively which this module would create, would be stored as secrets."
-  type        = string
-}
-
-variable "neon_app_role" {
-  description = "The name for the Neon role that the application will use."
-  type        = string
-  sensitive   = true
-}
-
-variable "neon_owner_role" {
-  description = "The name for the Neon owner role which would be used to connect to the specified database over psql (e.g. to run migrations)."
-  type        = string
-  sensitive   = true
-}
-
-
 
 # additional vars for flowmazonfrontend module
 variable "vercel_team_id" {
@@ -135,7 +77,15 @@ variable "secretname_for_vercel_project_id" {
   type        = string
 }
 
+variable "environmentname_for_secrets_and_variables" {
+  description = "Name of GitHub repo Environment in which to store project_id of the created vercel_project. In this environment the project_id would be stored as a secret with name specified by `secretname_for_vercel_project_id` argument."
+  type        = string
+}
 
+variable "repository_for_secrets_and_variables" {
+  description = "Name of GitHub repo in an an Environment of which (specified by `environmentname_for_vercel_project_id` argument) the project_id of the created vercel project would be stored. This should just be the repo's name, and NOT prefixed by account name or organisation name and NOT a fully qualified repo name either. Otherwise the repo may not be found. For example I would provide `flowmazondotnet` as the repo name instead of `EnableHub/flowmazondotnet` or `https://github.com/EnableHub/flowmazondotnet`."
+  type        = string
+}
 
 variable "grafanacloud_frontend_o11y_api_access_token" {
   description = "Access token for Grafana Cloud Frontend Observability API. This is used to create a new Frontend Observability instance in the Grafana Cloud stack. Obtain it as described here: https://registry.terraform.io/providers/grafana/grafana/latest/docs#managing-frontend-observability. I Would also use it as cloud_access_policy_token in the grafana provider block. Therefore it should additionally also have the following scopes: accesspolicies:read|write|delete, stacks:read|write|delete."
--- .iac/modules/db_branch/variables.tf
+++ .iac/modules/db_branch/variables.tf
@@ -1,45 +1,3 @@
-# Azure variables
-
-variable "managed_identity_name" {
-  description = "Name of the user-assigned managed identity to which read permissions would be assigned to the key vault secret that this module would create."
-  type        = string
-}
-
-variable "managed_identity_resource_group_name" {
-  description = "Name of the resource group in which the user-assigned managed identity exists."
-  type        = string
-}
-
-variable "vault_name" {
-  description = "The name of the key vault into which the key vault secrets crated by this module would be stored."
-  type        = string
-}
-
-variable "vault_resource_group_name" {
-  description = "The name of the resource group that contains the key vault."
-  type        = string
-}
-
-variable "vault_secretname_for_connectionstring" {
-  description = "name of the secret whose value is the connection string to be used by the API to connect to the database"
-  type        = string
-}
-
-variable "secretname_for_psql_owner_connectionstring" {
-  description = "Name of the GitHub Environment secret in which to store psql connection String that includes credentials of db owner account that would be used to connect to and execute queries (including DDL) on the database from psql."
-  type        = string
-}
-
-variable "environmentname_for_secrets_and_variables" {
-  description = "Name of GitHub repo Environment in which to store secrets and variables such as psql_owner_connection_string."
-  type        = string
-}
-
-variable "repository_for_secrets_and_variables" {
-  description = "Name of GitHub repo in an an Environment of which (specified by `environmentname_for_secrets_and_variables` argument) secrets and variables such as psql_owner_connection_string would be stored. This should just be the repo's name, and NOT prefixed by account name or organisation name and NOT a fully qualified repo name either. Otherwise the repo may not be found. For example I would provide `flowmazondotnet` as the repo name instead of `EnableHub/flowmazondotnet` or `https://github.com/EnableHub/flowmazondotnet`."
-  type        = string
-}
-
 # Neon DB variables
 
 variable "neon_project_id" {
@@ -55,24 +13,4 @@ variable "neon_source_branch_id" {
 variable "neon_new_branch_name" {
   description = "Name of the new branch that would be created by this module in the specified Neon DB Project."
   type        = string
-}
-
-variable "neon_database_name" {
-  description = "The name of the database in the specified Neon DB project. Permissions would be assigned in this database to the app role which this module would create on the new branch. Also, connection strings for connecting to this database, from app and owner role respectively which this module would create, would be stored as secrets."
-  type        = string
-}
-
-# for the following role names,
-# bear in mind that in NeonDB roles and users
-# are synonymous
-variable "neon_app_role" {
-  description = "The name for the Neon role that the application will use."
-  type        = string
-  sensitive   = true
-}
-
-variable "neon_owner_role" {
-  description = "The name for the Neon owner role which would be used to connect to the specified database over psql (e.g. to run migrations)."
-  type        = string
-  sensitive   = true
 }
