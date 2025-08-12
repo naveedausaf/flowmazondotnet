5de438d2e49875f0ba888b6c2ad71cb82919db54
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-08-12 19:06:13 +0100

    fix: changes (#142)

--- .iac/modules/aca_app/main.tf
+++ .iac/modules/aca_app/main.tf
@@ -63,6 +63,7 @@ resource "azurerm_key_vault_secret" "otlp_headers" {
 }
 
 resource "azurerm_key_vault_secret" "registry_password_or_token" {
+  count        = var.registry_username != null ? 1 : 0
   name         = var.vault_secretname_registry_password_or_token
   key_vault_id = data.azurerm_key_vault.vault.id
   value        = var.registry_password_or_token
@@ -129,11 +130,16 @@ resource "azurerm_container_app" "app" {
 
   }
 
-  secret {
-    name     = var.vault_secretname_registry_password_or_token
-    identity = data.azurerm_user_assigned_identity.managed_identity.id
-    # key_vault_secret_id is mandatory together with name above
-    key_vault_secret_id = azurerm_key_vault_secret.registry_password_or_token.id
+  dynamic "secret" {
+
+    for_each = var.registry_username != null ? [1] : []
+
+    content {
+      name     = var.vault_secretname_registry_password_or_token
+      identity = data.azurerm_user_assigned_identity.managed_identity.id
+      # key_vault_secret_id is mandatory together with name above
+      key_vault_secret_id = azurerm_key_vault_secret.registry_password_or_token[0].id
+    }
   }
 
   # like the vault_secretname_env_OTEL_EXPORTER_OTLP_HEADERS
