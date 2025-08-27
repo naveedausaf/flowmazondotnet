41045090d69a58b12d32c9701c1eca0ebe6efead
naveedausaf <20386346+naveedausaf@users.noreply.github.com>
2025-08-27 12:07:36 +0100

    fix: upload junit report for playwright

--- flowmazonfrontend/playwright.config.ts
+++ flowmazonfrontend/playwright.config.ts
@@ -23,7 +23,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html', { open: 'never' }]],
+  reporter: process.env.CI
+    ? [
+        ['html', { open: 'never' }],
+        ['junit', { outputFile: 'junit-report/test_results.xml' }],
+      ]
+    : [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
